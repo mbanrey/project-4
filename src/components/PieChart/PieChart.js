39dae9193654c0f51cecda088303ee2d21a3ff67
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-03-10 10:18:27 -0500

    render pie chart with user expenses.
    Co-authored-by: Mitchell Banrey <mitchellbanrey@gmail.com>

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -17,33 +17,48 @@ ChartJS.register(
 
 
 
-export default function PieChart() {
+export default function PieChart({expenses}) {
+    const labelMap = {
+        'gas': 0,
+        'bills': 1,
+        'food': 2,
+        'vehicle': 3,
+        'entertainment': 4,
+        'travel': 5
+    }
+
+
     const data = {
-    labels: ['Gas', 'Bills', 'Food', 'Vehicle', 'Entertainment', 'Travel'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+        labels: ['Gas', 'Bills', 'Food', 'Vehicle', 'Entertainment', 'Travel'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [0, 0, 0, 0, 0, 0],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
         ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+      };
+
+      for(let expense of expenses){
+        data.datasets[0].data[labelMap[expense.category]] += expense.amount
+      }
+
     return (
         <div className='pie'>
             <h1>Hello This is Pie</h1>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,16 +1,15 @@
 import { useState } from "react"
 import PieChart from "../../components/PieChart/PieChart"
 
-function HomePage({userExpenses}) {
-  const [expenses, setExpenses] = useState(userExpenses)
+function HomePage({userExpenses, setExpenses}) {
 
   return (
     <div>
-      {expenses && expenses.map((expense, index) => (
+      {userExpenses && userExpenses.map((expense, index) => (
         <div key={index}>{expense.amount}</div>
       ))}
       <div className="chia">
-      <PieChart className={'pie'}/>
+      <PieChart expenses={userExpenses} className={'pie'}/>
       </div>
     </div>
   )
